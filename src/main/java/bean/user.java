1116d49f97de90f676535db464b2a0a2950a9fab
huidu <huidu@hh.com>
2019-07-26 12:36:10 +0800

    在2中添加其他属性

--- src/main/java/bean/user.java
+++ src/main/java/bean/user.java
@@ -1,5 +1,22 @@
 package bean;
 
 public class user {
+	private String password;
+	private int code;
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
 }
