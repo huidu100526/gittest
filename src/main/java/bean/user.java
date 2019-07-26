324f7d4c237cb4b6253a95748c25e56f5b373202
huidu <huidu@hh.com>
2019-07-26 12:34:46 +0800

    添加类属性

--- src/main/java/bean/user.java
+++ src/main/java/bean/user.java
@@ -1,5 +1,22 @@
 package bean;
 
 public class user {
+	private int userid;
+	private String username;
 
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
